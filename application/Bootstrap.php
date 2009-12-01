c24627ff0377ecb1089f9af0764a967d3e331067
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2009-12-01 16:32:22 +0100

    Nu autoloader zend også models fra library

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,11 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    function _initAutoloader(){
+        require_once 'Zend/Loader/Autoloader.php';
+        $loader = Zend_Loader_Autoloader::getInstance();
+        $loader->registerNamespace('Model_');
+    }
 
 }
 
